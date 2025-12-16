91a7a8e1ec9b362861a035ff0b4b5a3488a8ff94
Jose Maria Farfan <magoalakai@gmail.com>
2025-12-16 20:32:17 +0100

    Testing CI/CD

--- 5. Bank.Notification/Bank.Notification.Api/Applicacion/Features/Process/ProcessService.cs
+++ 5. Bank.Notification/Bank.Notification.Api/Applicacion/Features/Process/ProcessService.cs
@@ -19,6 +19,7 @@ public class ProcessService(INotificationDbContext notificationDbContext, ISendG
 
             string status = notificationEntity.TransactionState ? "successful" : "failed";
             emailPayload = CreateSendGridModel.Create(toEmail, fromEmail, status, notificationEntity.Content);
+            Console.WriteLine(emailPayload);
         }
         else
         {
